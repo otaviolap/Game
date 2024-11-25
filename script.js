bd44c536558af2850657d852f928e6b1a84ebe7e
otaviolap <otaviolapbrandi@hotmail.com>
2024-11-25 19:49:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,7 @@ let blackJackGame = {
     "computador":{'spanPontuacao':'#computador-pontuacao','div':'#computador-box','pontuacao':0},
     "cartas":['2','3','4','5','6','7','8','9','10','K','J','Q','A'],
     "cartasPontos": {'2':2, '3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10, 'K':10, 'J':10,'Q':10,'A':[1,11]},
-    "vitorias":0,
-    "derrotas":0,
-    "empates":0,
-    "isStand":false,
-    "turnsOver":false,
+
   }
   function login() {
     const name = document.getElementById("name").value;
